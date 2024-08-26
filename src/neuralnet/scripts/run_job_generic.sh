92d2781341e8ca4748e2e90f8c1d6f1d219ce15f
Rahul Padmanabhan <rahul.padmanabhan@mail.concordia.ca>
2024-08-25 23:49:18 -0400

    added fixes

--- src/neuralnet/scripts/run_job_generic.sh
+++ src/neuralnet/scripts/run_job_generic.sh
@@ -8,6 +8,8 @@
 #SBATCH --time=1-00:00
 #SBATCH --mail-user=rahul.padmanabhan@mail.concordia.ca
 #SBATCH --mail-type=ALL
+#SBATCH --output=/home/rahul3/temp/slurm_output/%x_%j.out
+#SBATCH --error=/home/rahul3/temp/slurm_output/%x_%j.err
 
 # Run the bashrc first
 . ~/.bashrc
@@ -44,7 +46,7 @@ elif [ "$(hostname)" = "node2" ]; then
     echo "Using local node2"
     printf '%*s\n' 25 '' | tr ' ' '*'
     export LAWT_DUMP_PATH="/home/rahulpadmanabhan/Development/ws1/masters_thesis_2/experiments"
-elif [ $CLUSTER = "beluga" ] || [ $CLUSTER = "graham" ]; then
+elif [ "${CLUSTER:-}" = "beluga" ] || [ "${CLUSTER:-}" = "graham" ]; then
     printf '%*s\n' 25 '' | tr ' ' '*'
     echo "Using the ${CLUSTER} cluster"
     printf '%*s\n' 25 '' | tr ' ' '*'
@@ -77,7 +79,7 @@ echo "PYTHON_REQUIREMENTS: $PYTHON_REQUIREMENTS"
 
 # Experiment details
 EXP_NAME="matrix_logarithm"
-EXP_ID="202408251204"
+EXP_ID="202408252325"
 OPERATION="matrix_logarithm"
 MAX_EPOCHS=100
 
@@ -89,8 +91,13 @@ echo "Operation: $OPERATION"
 echo "Displaying the result of 'which python' command:"
 which python
 
-if [ $CLUSTER = "beluga" ] || [ $CLUSTER = "graham" ]; then
-    srun python $LAWT_TRAIN --dump_path "${LAWT_DUMP_PATH}" --save_periodic 0 --fp16 true --amp 2 --accumulate_gradients 1 --clip_grad_norm 5 --enc_emb_dim 512 --dec_emb_dim 512 --n_enc_layers 8 --n_dec_layers 1 --n_enc_heads 8 --n_dec_heads 8 --dropout 0 --attention_dropout 0 --share_inout_emb true --sinusoidal_embeddings false --optimizer 'adam_warmup,warmup_updates=10000,lr=0.0001' --batch_size 64 --batch_size_eval 128 --max_len 200 --epoch_size 300000 --max_epoch $MAX_EPOCHS --num_workers 10 --export_data false --reload_data '' --reload_size '-1' --batch_load false --env_name numeric --tasks numeric --env_base_seed '-1' --eval_size 10000 --min_dimension 5 --max_dimension 5 --max_input_coeff 5 --operation $OPERATION --generator gaussian --rectangular false --output_encoding 'floatsymbol,2' --input_encoding 'floatsymbol,2' --max_output_len 80 --float_tolerance '0.05' --more_tolerance '0.02,0.01,0.005' --eval_norm d1 --eval_verbose 0 --beam_eval 1 --stopping_criterion 'valid_numeric_beam_acc,60' --validation_metrics valid_numeric_beam_acc --exp_name "${EXP_NAME}" --exp_id "${EXP_ID}"
+if [ "${CLUSTER:-}" = "beluga" ] || [ "${CLUSTER:-}" = "graham" ]; then
+    echo "Python version:"
+    python --version
+    PYTHON_EXEC=$(which python)
+    echo "Python path: $PYTHON_EXEC"
+
+    srun $PYTHON_EXEC $LAWT_TRAIN --dump_path "${LAWT_DUMP_PATH}" --save_periodic 0 --fp16 true --amp 2 --accumulate_gradients 1 --clip_grad_norm 5 --enc_emb_dim 512 --dec_emb_dim 512 --n_enc_layers 8 --n_dec_layers 1 --n_enc_heads 8 --n_dec_heads 8 --dropout 0 --attention_dropout 0 --share_inout_emb true --sinusoidal_embeddings false --optimizer 'adam_warmup,warmup_updates=10000,lr=0.0001' --batch_size 64 --batch_size_eval 128 --max_len 200 --epoch_size 300000 --max_epoch $MAX_EPOCHS --num_workers 10 --export_data false --reload_data '' --reload_size '-1' --batch_load false --env_name numeric --tasks numeric --env_base_seed '-1' --eval_size 10000 --min_dimension 5 --max_dimension 5 --max_input_coeff 5 --operation $OPERATION --generator gaussian --rectangular false --output_encoding 'floatsymbol,2' --input_encoding 'floatsymbol,2' --max_output_len 80 --float_tolerance '0.05' --more_tolerance '0.02,0.01,0.005' --eval_norm d1 --eval_verbose 0 --beam_eval 1 --stopping_criterion 'valid_numeric_beam_acc,60' --validation_metrics valid_numeric_beam_acc --exp_name "${EXP_NAME}" --exp_id "${EXP_ID}"
 else
     python $LAWT_TRAIN --dump_path "${LAWT_DUMP_PATH}" --save_periodic 0 --fp16 true --amp 2 --accumulate_gradients 1 --clip_grad_norm 5 --enc_emb_dim 512 --dec_emb_dim 512 --n_enc_layers 8 --n_dec_layers 1 --n_enc_heads 8 --n_dec_heads 8 --dropout 0 --attention_dropout 0 --share_inout_emb true --sinusoidal_embeddings false --optimizer 'adam_warmup,warmup_updates=10000,lr=0.0001' --batch_size 64 --batch_size_eval 128 --max_len 200 --epoch_size 300000 --max_epoch $MAX_EPOCHS --num_workers 10 --export_data false --reload_data '' --reload_size '-1' --batch_load false --env_name numeric --tasks numeric --env_base_seed '-1' --eval_size 10000 --min_dimension 5 --max_dimension 5 --max_input_coeff 5 --operation $OPERATION --generator gaussian --rectangular false --output_encoding 'floatsymbol,2' --input_encoding 'floatsymbol,2' --max_output_len 80 --float_tolerance '0.05' --more_tolerance '0.02,0.01,0.005' --eval_norm d1 --eval_verbose 0 --beam_eval 1 --stopping_criterion 'valid_numeric_beam_acc,60' --validation_metrics valid_numeric_beam_acc --exp_name "${EXP_NAME}" --exp_id "${EXP_ID}"
     echo "Experiment path: ${LAWT_DUMP_PATH}/${EXP_NAME}/${EXP_ID}"
