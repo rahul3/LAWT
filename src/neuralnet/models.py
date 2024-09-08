cede96369ed10666dca8e4d9ece16ec5e8ef7ba7
Rahul Padmanabhan <rahul.padmanabhan@mail.concordia.ca>
2024-09-08 13:39:22 -0400

    Added transformer encoder, deepmatrixnet

--- src/neuralnet/models.py
+++ src/neuralnet/models.py
@@ -16,3 +16,48 @@ class MatrixNet(nn.Module):
         x = self.relu(self.fc3(x))
         x = self.fc4(x)
         return x
+    
+    
+class DeepMatrixNet(nn.Module):
+    def __init__(self, input_size):
+        super(DeepMatrixNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, 128)
+        self.fc2 = nn.Linear(128, 256)
+        self.fc3 = nn.Linear(256, 512)
+        self.fc4 = nn.Linear(512, 1024)
+        self.fc5 = nn.Linear(1024, 512)
+        self.fc6 = nn.Linear(512, 256)
+        self.fc7 = nn.Linear(256, 128)
+        self.fc8 = nn.Linear(128, input_size)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.2)
+
+    def forward(self, x):
+        x = self.dropout(self.relu(self.fc1(x)))
+        x = self.dropout(self.relu(self.fc2(x)))
+        x = self.dropout(self.relu(self.fc3(x)))
+        x = self.dropout(self.relu(self.fc4(x)))
+        x = self.dropout(self.relu(self.fc5(x)))
+        x = self.dropout(self.relu(self.fc6(x)))
+        x = self.dropout(self.relu(self.fc7(x)))
+        x = self.fc8(x)
+        return x
+
+
+class MatrixFunctionTransformer(nn.Module):
+    def __init__(self, d_model, nhead, num_layers):
+        super().__init__()
+        self.encoder1 = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(d_model, nhead, dim_feedforward=d_model*4, dropout=0.1, batch_first=True),
+            num_layers
+        )
+        self.encoder2 = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(d_model, nhead, dim_feedforward=d_model*4, dropout=0.1, batch_first=True),
+            num_layers
+        )
+
+    def forward(self, x):
+        x = self.encoder1(x)
+        x = x.transpose(-2, -1)
+        x = self.encoder2(x)
+        return x
