65864a1c056b96343957d9cee826793d45d02ee1
Rahul Padmanabhan <rahul.padmanabhan@mail.concordia.ca>
2026-01-17 00:27:16 -0500

    Added deploy script. Added additional experiment work.

---
+++ deploy/run_shallow_nn.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#SBATCH --account=def-sbrugiap
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=3
+#SBATCH --mem=16G
+#SBATCH --time=1-00:00
+#SBATCH --mail-user=rahul.padmanabhan@mail.concordia.ca
+#SBATCH --mail-type=ALL
+#SBATCH --output=/home/rahul3/scratch/2026/slurm_output/%x_%j.out
+#SBATCH --error=/home/rahul3/scratch/2026/slurm_output/%x_%j.err
+
+# Run the bashrc first
+. ~/.bashrc
+export SHALLOW_NN_TRAIN="/home/rahul3/projects/def-sbrugiap/rahul3/icprai_2026/LAWT/src/neuralnet/deepnetwork_hd_train.py"
+export LAWT_PATH="/home/rahul3/projects/def-sbrugiap/rahul3/icprai_2026/LAWT"
+export LAWT_DUMP_PATH="/home/rahul3/scratch/experiments"
+export PYTHON_REQUIREMENTS="/home/rahul3/projects/def-sbrugiap/rahul3/icprai_2026/LAWT/requirements.txt"
+
+echo "LAWT_PATH: $LAWT_PATH"
+echo "LAWT_DUMP_PATH: $LAWT_DUMP_PATH"
+echo "PYTHON_REQUIREMENTS: $PYTHON_REQUIREMENTS"
+
+printf '%*s\n' 25 '' | tr ' ' '*'
+echo "Using the ${CLUSTER} cluster"
+printf '%*s\n' 25 '' | tr ' ' '*'
+
+module load python/3.10
+module load scipy-stack
+export SLURM_TMPDIR="/home/rahul3/projects/def-sbrugiap/rahul3/icprai_2026/slurm_tmpdir"
+echo "SLURM_TMPDIR: $SLURM_TMPDIR"
+
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+
+# echo "Starting to install requirements...."
+# pip install --no-index -r $PYTHON_REQUIREMENTS
+
+pip install --no-index torch torchvision torchaudio
+pip install --no-index 'numpy<2.0'
+
+
+# Experiment details
+EXP_NAME="shallownetwork"
+EXP_ID="$(date +"%Y%m%d%H%M")"
+OPERATION="log"
+MATRIX_TYPE="general"
+MAX_EPOCHS=100
+
+echo "Experiment name: $EXP_NAME"
+echo "Experiment ID: $EXP_ID"
+echo "Max epochs: $MAX_EPOCHS"
+echo "Operation: $OPERATION"
+
+echo "Displaying the result of 'which python' command:"
+which python
+
+echo "Running with srun"
+echo "Python version:"
+python --version
+PYTHON_EXEC=$(which python)
+echo "Python path: $PYTHON_EXEC"
+
+
+EXP_ID="$MATRIX_TYPE_${OPERATION}_${ENCODING_NAME}_dim_${MATRIX_DIM}_$(date +"%Y%m%d%H%M")"
+
+srun $PYTHON_EXEC $SHALLOW_NN_TRAIN 
--- src/neuralnet/deepnetwork_hd_train.py
+++ src/neuralnet/deepnetwork_hd_train.py
@@ -1,8 +1,8 @@
-import os
 import argparse
 import csv
 import datetime
 import logging
+import os
 
 import warnings
 warnings.filterwarnings('ignore', message='logm result may be inaccurate')
@@ -13,6 +13,7 @@ import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.data import random_split
 import numpy as np
+import pandas as pd
 import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -37,10 +38,7 @@ operations_to_run = ["log"]
 for operation in operations_to_run:
     # Generate ID based on current datetime
     ID = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-    # ###########################################################################################
-    # operation = "sin"
-    # ###########################################################################################
-    save_dir = f"/home/rahulpadmanabhan/Development/ws1/experiments/deepnetwork/{operation}/deepmatrixnet_{ID}"
+    save_dir = f"/home/rahul3/scratch/2026/experiments/shallownetwork/{operation}/shallow_matrixnet_{ID}"
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     logger = get_logger(__name__, log_file=os.path.join(save_dir, f"{operation}_{ID}.log"))
@@ -51,14 +49,13 @@ for operation in operations_to_run:
     logger.info(f"Experiment ID: {ID}")
 
 
-
     logger.info(f"Experiment ID: {ID}")
     logger.info(f"Operation: {operation}")
 
     for dim in range(1, 8):
         distribution = "gaussian" if operation != "log" else "gaussian_positive"
-        coeff_lower = -1
-        coeff_upper = 1
+        coeff_lower = -5
+        coeff_upper = 5
         batch_size = 128
         num_epochs = 100
         validation_interval = 10
@@ -136,9 +133,12 @@ for operation in operations_to_run:
                     
                     
             model_save_dir = os.path.join(save_dir, f"dim_{dim}")
+            logger.info(f"Saving model to {model_save_dir}")
             if not os.path.exists(model_save_dir):
                 os.makedirs(model_save_dir)
-            torch.save(model.state_dict(), os.path.join(model_save_dir, f'{operation}_model_{str(train_sample)}.pth'))
+            model_save_dict_path = os.path.join(model_save_dir, f'{operation}_model_{str(train_sample)}.pth')
+            torch.save(model.state_dict(), model_save_dict_path)
+            logger.info(f"Model saved to {model_save_dict_path}")
 
             # Training loop complete
             logger.info("-" * 50)
@@ -149,8 +149,6 @@ for operation in operations_to_run:
             
             model.eval()
             test_loss = 0
-            test_loss_real = 0
-            test_loss_imag = 0
             with torch.no_grad():
                 for test_idx, (x, y) in enumerate(test_loader):
                     if dim == 1:
@@ -162,7 +160,7 @@ for operation in operations_to_run:
                     
                     output = model(x)
                     test_loss += criterion(output, y).item()
-                    # logger.info(f"Test: Batch {test_idx+1}/{len(test_loader)}, Current Test Batch Loss: {criterion(test_output, y).item():.4f}, Cumulative Test Loss: {test_loss:.4f}")
+                    logger.info(f"Test: Batch {test_idx+1}/{len(test_loader)}, Current Test Batch Loss: {criterion(output, y).item():.4f}, Cumulative Test Loss: {test_loss:.4f}")
 
                 # Compare a few results
                 for i in range(3):
@@ -173,6 +171,9 @@ for operation in operations_to_run:
 
             test_loss /= len(test_loader)
             logger.info(f'Test Loss: {test_loss:.4f}')
+
+            # Collect results in a list instead of appending to DataFrame
+            results_list = []
 
             with torch.no_grad():
                 # create 10000 evaluation samples
@@ -204,9 +205,28 @@ for operation in operations_to_run:
                         correct_predictions[tol] = (error / (torch.abs(y) + 1e-12)) <= tol
                         evaluations_correct[tol] += correct_predictions[tol].sum().item()
                         logger.info(f"Evaluation {evaluation_idx+1}/{evaluation_samples}, Correct predictions: {correct_predictions[tol].sum().item()}/{x.size(0)}, Tolerance: {tol}")
+                        
                 for tol in tols:
                     logger.info(f"Total correct predictions: {evaluations_correct[tol]}/{evaluation_samples}, Tolerance: {tol}")
                     logger.info(f"Accuracy: {evaluations_correct[tol]/evaluation_samples:.4f}, Tolerance: {tol}")
+                    accuracy = evaluations_correct[tol]/evaluation_samples
+                    results_list.append({
+                        "operation": operation,
+                        "dim": dim,
+                        "train_sample": train_sample,
+                        "test_samples": test_samples,
+                        "num_epochs": num_epochs,
+                        "lr": lr,
+                        "error": error.mean().item(),
+                        "tolerance": tol,
+                        "correct_predictions": evaluations_correct[tol]/evaluation_samples,
+                        "accuracy": accuracy
+                    })
+            
+            # Create DataFrame from the list
+            df = pd.DataFrame(results_list)
+            df.to_csv(os.path.join(model_save_dir, f'{operation}_results_{str(train_sample)}.csv'), index=False)
+            logger.info(f"Results saved to {os.path.join(model_save_dir, f'{operation}_results_{str(train_sample)}.csv')}")
 
     logger.info("-" * 50)
     logger.info("Experiment complete")
